87b9f081d0831ba6afb38358d79f686c1c108d3a
Austin <trigger_austin00@proton.me>
2024-03-28 13:30:26 -0700

    stack_manip_op.c edit

--- stack_math_op.c
+++ stack_math_op.c
@@ -3,7 +3,7 @@
 /**
  * _add - adds two top values of the stack
  * @head: pointer to head of stack
- * 
+ * @count: line number in monty file
  */
  void _add(stack_t **head, unsigned int count)
 {
@@ -14,86 +14,90 @@
 		fprintf(stderr, "L%u: can't add, stack too short\n", count);
 		exit(EXIT_FAILURE);
 	}
-    /*take first two values*/
-    val1 = *head;
-    val2 =  (*head)->next;
-    
-    val1->n += val2->n;/*add values*/
-    free(val2);
-    /*put results to top of stack*/
-    *head = val1;
+
+	/*take first two values*/
+	val1 = *head;
+	val2 =  (*head)->next;
+
+	val1->n += val2->n;/*add values*/
+	free(val2);
+	
+	/*put results to top of stack*/
+	*head = val1;
 }
 
 /**
  * _sub - subtracts the top two values of the stack
  * @head: pointer to top of stack
- * 
+ * @count: line number in monty file
  */
 void _sub(stack_t **head, unsigned int count)
 { 
-    stack_t *val1, *val2;
-    
-    if(!*head || !(*head)->next)
+	stack_t *val1, *val2;
+
+	if(!*head || !(*head)->next)
 	{
 		fprintf(stderr, "L%u: can't subtract, stack too short\n", count);
 		exit(EXIT_FAILURE);
 	}
-    
-    /*take two values from top*/
-    val1 = *head;
-    val2 = (*head)->next;
-    
-    val1->n -= val2->n;
-    *head = val1;
+
+	/*take two values from top*/
+	val1 = *head;
+	val2 = (*head)->next;
+
+	val1->n -= val2->n;
+	*head = val1;
 }
 
 /**
- * 
- * 
+ * _div - divides top two values in a stack
+ * @head: pointer to head of stack
+ * @count: line number in monty file
  */
 void _div(stack_t **head, unsigned int count)
 {
-    stack_t *val1, *val2;
-    
-    if(!*head || !(*head)->next)
+	stack_t *val1, *val2;
+
+	if(!*head || !(*head)->next)
 	{
 		fprintf(stderr, "L%u can't divide, stack too short", count);
 		exit(EXIT_FAILURE);
 	}
-	
-	/*take two values from top*/
-    val1 = *head;
-    val2 = (*head)->next;
-    if(val2->n == 0)
-    {
-        fprintf(stderr, "l%u, zero division", count);
-        exit(EXIT_FAILURE);
-    }
-    
-    val1->n /= val2->n;
-    free(val2);
-    *head = val1;
+
+    	/*take two values from top*/
+	val1 = *head;
+	val2 = (*head)->next;
+
+	if(val2->n == 0)
+	{
+		fprintf(stderr, "l%u, zero division", count);
+		exit(EXIT_FAILURE);
+	}
+
+	val1->n /= val2->n;
+	free(val2);
+	*head = val1;
 }
 
 /**
- * 
- * 
- * 
+ * _mul - multiplies top two stack values
+ * @head: pointer to head of stack
+ * @count: line number in monty file
  */
 void _mul(stack_t **head, unsigned int count)
 {
-    stack_t *val1, *val2;
+	stack_t *val1, *val2;
     
-    if(!*head || !(*head)->next)
+	if(!*head || !(*head)->next)
 	{
 		fprintf(stderr, "L%u: can't multiply, stack too short", count);
 		exit(EXIT_FAILURE);
 	}
-	
+
 	/*take two values from top*/
-    val1 = *head;
-    val2 = (*head)->next;
-    
-    val1->n *= val2->n;
-    *head = val1;
+	val1 = *head;
+	val2 = (*head)->next;
+
+	val1->n *= val2->n;
+	*head = val1;
 }
