483085d166c8a19e61b3eeac0a565f663433c445
Austin <trigger_austin00@proton.me>
2024-03-31 12:47:22 -0700

    fixed val.c check null value

--- val.c
+++ val.c
@@ -7,17 +7,20 @@
  */
 int _isint(char *value)
 {
-    int j = 0, checker;
-    if(value[0] == '-')
-    {
-        j++;
-    }
-    for(; value[j] != '\0'; j++)
-    {
-        if(value[j] <= 57 && value[j] >= 48)
-            checker = 1;
-        else
-          checker = 0;
-    }
-    return (checker);
+	if (value == NULL)
+		return 0;
+
+	int j = 0, checker;
+	if(value[0] == '-')
+	{
+		j++;
+	}
+	for(; value[j] != '\0'; j++)
+	{
+		if(value[j] <= 57 && value[j] >= 48)
+			checker = 1;
+		else
+			checker = 0;
+	}
+	return (checker);
 }
